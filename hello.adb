574c764591c854a07931f7454e496c5dd342e8a0
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-10-08 19:16:33 +0530

    Create hello.adb

---
+++ hello.adb
@@ -0,0 +1,9 @@
+with Ada.Text_IO;
+
+procedure Hello_World is
+   use Ada.Text_IO;
+begin
+   Put_line ("Hello World");
+end Hello_World;
+
+--Ada is a structured, statically typed, imperative, and object-oriented high-level programming language, extended from Pascal and other languages. 
