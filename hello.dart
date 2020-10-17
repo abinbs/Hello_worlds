0bb0d8755e5824d21b914233c0452821251e320e
Abin Binu Sam <40056513+abinbs@users.noreply.github.com>
2020-10-17 12:17:03 +0530

    Create hello.dart

---
+++ hello.dart
@@ -0,0 +1,5 @@
+main() {
+  print('Hello World');
+}
+
+//Dart is a client-optimized programming language for apps on multiple platforms.Dart is an object-oriented, class-based, garbage-collected language with C-style syntax
